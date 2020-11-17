5f4544bb5bc524b6843af10da1ad7671a9b0e947
罗洪盼 <hongpan.luo@powersi.com.cn>
2020-11-17 16:07:09 +0800

    20/11/17入门测试

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @author 医保产品二部_罗洪盼_CN32230
+ * @Description:
+ * @create 2020-11-17 15:42
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hi SB");
+    }
+}
